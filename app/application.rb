167f2506de086c2e34a69e479c13aba51921ca0c
Elias Ventura <eliasventura23@gmail.com>
2019-02-02 20:12:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,11 +4,11 @@ class Application
     resp = Rack::Response.new
 
     if hour < 12
-      resp.write "Good Morning"
+      resp.write "Good Morning!"
     elsif hour > 12
-      resp.write "Good Afternoon"
+      resp.write "Good Afternoon!"
     end
-    
+
     resp.finish
   end
 
